7e408dc71fd91e5b5ed1f6224aadf97fae18eca9
Marcelo Tambalo <marcelo.zokis@gmail.com>
2015-06-10 17:55:35 -0300

    command line

--- sgpm.go
+++ sgpm.go
@@ -10,7 +10,36 @@ import (
   "github.com/zokis/gopassgen"
   "github.com/atotto/clipboard"
   "github.com/gcmurphy/getpass"
+  "gopkg.in/alecthomas/kingpin.v2"
   "code.google.com/p/go.crypto/blowfish"
+)
+
+const (
+  _GET = "get"
+  _NEW = "new"
+  _FIND = "find"
+  _DEL = "del"
+  _GEN = "gen"
+)
+
+var (
+  app = kingpin.New("sgpm", "Simple Go Password Manager")
+
+  cGet = app.Command("get", "get")
+  gKey = cGet.Arg("key", "Key to get a password").String()
+
+  cNew = app.Command("new", "new password")
+  nKey = cNew.Arg("key", "Key for the new password").String()
+
+  cFind = app.Command("find", "find a key")
+  fKey = cFind.Arg("key", "Key to find").String()
+
+  cDel = app.Command("del", "delete")
+  dKey = cDel.Arg("key", "Key to delete a password").String()
+
+  cGen = app.Command("gen", "gen password")
+  gLen = cGen.Arg("length", "password length").Default("13").Int()
+
 )
 
 func blowfishChecksizeAndPad(pt []byte) []byte {
@@ -64,56 +93,26 @@ func getSecretKey(c int) string {
   return secretkey
 }
 
-func stringInSlice(s string, list []string) bool {
-  for k, e := range list {
-    if e == s {
-      return k + 1
-    }
-  }
-  return 0
-}
-
 func main() {
-  var aciton, key, norKey, path, securityKey, securityPass string
-  acitonOk := false
-  copy := true
-  keyInt := 13
-  _C := "-C"
-  _E := ""
-  _DEL := "del"
-  _FIND := "find"
-  _GEN := "gen"
-  _GET := "get"
-  _NEW := "new"
-  actions := []string{_DEL, _FIND, _GEN, _GET, _NEW}
-  args := os.Args[1:]
+  var aciton, key, norKey, path, secretkey, securityKey, securityPass string
+  switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+    case cGet.FullCommand():
+      aciton = _GET
+      key = string(*gKey)
+    case cNew.FullCommand():
+      aciton = _NEW
+      key = string(*nKey)
+    case cFind.FullCommand():
+      aciton = _FIND
+      key = string(*fKey)
+    case cDel.FullCommand():
+      aciton = _DEL
+      key = string(*dKey)
+    case cGen.FullCommand():
+      fmt.Printf("%s\n", gopassgen.NewPassword(gopassgen.OptionLength(*gLen)))
+      os.Exit(0)
+  }
 
-  if stringInSlice(_C, args) > 0 {
-    copy = false
-  }
-  if len(args) >= 1 {
-    aciton = args[0]
-    if stringInSlice(aciton, actions) > 0 {
-      acitonOk = true
-    }
-    if len(args) >= 2 {
-      key = args[1]
-      if key == _C {
-        key = _E
-      }
-    }
-  }
-  if aciton == _GEN {
-    if key != _E {
-      var err error
-      keyInt, err = strconv.Atoi(key)
-      if err != nil {
-        keyInt = 13
-      }
-    }
-    fmt.Printf("%s\n", gopassgen.NewPassword(gopassgen.OptionLength(keyInt)))
-    os.Exit(0)
-  }
   defaultDB := "sgpm.dwarf"
   if path = os.Getenv("SGPM_DB_PATH"); len(path) <= 0 {
     fmt.Printf("Database [" + defaultDB + "]: ")
@@ -126,45 +125,44 @@ func main() {
   if norKey = os.Getenv("SGPM_NOR_KEY"); len(norKey) <= 0 {
     norKey = "0x2b4674dca78cfde8d3d5a0d100996c941038ef71100"
   }
-  if securityPass = os.Getenv("SGPM_PASS"); len(securityPass) <= 0 {
-    securityPass = "0x27690b1e0e5bf93eb514035c824b85d3c274c"
-  }
   if securityKey = os.Getenv("SGPM_KEY"); len(securityKey) <= 0 {
     securityKey = "0x117498f0ea387cea4b00f77e8693ff9367a6L6"
   }
-  secretkey := getSecretKey(0)
+  if securityPass = os.Getenv("SGPM_PASS"); len(securityPass) <= 0 {
+    securityPass = "0x27690b1e0e5bf93eb514035c824b85d3c274c"
+  }
   pass, err := ddb.Get(securityKey)
   if err == nil {
+    secretkey = getSecretKey(0)
     if !strings.Contains(decryptPass(pass.(string), secretkey), securityPass) {
       nor, norErr := ddb.Get(norKey)
       norStr := nor.(string)
+      delDB := false
       if norErr == nil {
         if norStr == "3" {
-          if ddb.DelDB() {
-            fmt.Println("the database was destroyed")
-          }
+          delDB = true
         } else {
           newNor, atoiErr := strconv.Atoi(norStr)
           if atoiErr == nil{
             ddb.Set(norKey, strconv.Itoa(newNor + 1))
           } else {
-            ddb.Set(norKey, "1")
+            delDB = true
           }
         }
+      }
+      if delDB {
+        ddb.DelDB()
+        fmt.Println("the database was destroyed")
       }
       os.Exit(0)
     }
   } else {
     fmt.Println("New Database")
+    secretkey = getSecretKey(1)
     ddb.Set(securityKey, encryptPass(securityPass, secretkey))
   }
   ddb.Set(norKey, "0")
-  for !acitonOk {
-    fmt.Printf("Aciton [" + strings.Join(actions, " ") + "]: ")
-    fmt.Scanf("%s", &aciton)
-    acitonOk = stringInSlice(aciton, actions) > 0
-  }
-  if aciton != _GEN && key == _E {
+  if key == "" {
     fmt.Printf("Key: ")
     fmt.Scanf("%s", &key)
   }
@@ -180,12 +178,8 @@ func main() {
     pass, err := ddb.Get(key)
     if err == nil {
       dp := decryptPass(pass.(string), secretkey)
-      if copy {
-        if err := clipboard.WriteAll(string(dp)); err != nil {
-          fmt.Println("an error occurred while copying the password to the clipboard")
-        }
-      } else {
-        fmt.Println(dp)
+      if err := clipboard.WriteAll(dp); err != nil {
+        fmt.Println("an error occurred while copying the password to the clipboard")
       }
     }
   } else if aciton == _NEW {
@@ -198,8 +192,6 @@ func main() {
     ddb.Set(key, encryptPass(pass, secretkey))
   } else if aciton == _DEL {
     ddb.Rem(key)
-  } else if aciton == _GEN {
-    fmt.Printf("%s\n", gopassgen.NewPassword(gopassgen.OptionLength(13)))
   }
   os.Exit(0)
 }
