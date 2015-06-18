aca7922a5ed25f39ddd75d4f5b1880861d2480a2
Marcelo Tambalo <marcelo.zokis@gmail.com>
2015-06-18 18:03:48 -0300

    rc4

--- sgpm.go
+++ sgpm.go
@@ -5,6 +5,7 @@ import (
   "fmt"
   "strconv"
   "strings"
+  "encoding/hex"
   "crypto/cipher"
   "github.com/zokis/dwarfdb"
   "github.com/zokis/gopassgen"
@@ -20,6 +21,7 @@ const (
   _FIND = "find"
   _DEL = "del"
   _GEN = "gen"
+  _CHANGE = "0x405f52f666146d5e05c51d1e0e245bd9"
 )
 
 var (
@@ -39,34 +41,7 @@ var (
 
   cGen = app.Command("gen", "gen password")
   gLen = cGen.Arg("length", "password length").Default("13").Int()
-
 )
-
-func cipherC(text string, direction int) string {
-    shift, offset := rune(25), rune(26)
-    runes := []rune(text)
-    for index, char := range runes {
-        switch direction {
-        case -1:
-            if char >= 'a'+shift && char <= 'z' || char >= 'A'+shift && char <= 'Z' {
-                char = char - shift
-            } else if char >= 'a' && char < 'a'+shift || char >= 'A' && char < 'A'+shift {
-                char = char - shift + offset
-            }
-        case +1:
-            if char >= 'a' && char <= 'z'-shift || char >= 'A' && char <= 'Z'-shift {
-                char = char + shift
-            } else if char > 'z'-shift && char <= 'z' || char > 'Z'-shift && char <= 'Z' {
-                char = char + shift - offset
-            }
-        }
-        runes[index] = char
-    }
-    return string(runes)
-}
-
-func encode(text string) string { return cipherC(text, -1) }
-func decode(text string) string { return cipherC(text, +1) }
 
 func blowfishChecksizeAndPad(pt []byte) []byte {
   mod := len(pt) % blowfish.BlockSize
@@ -77,7 +52,6 @@ func blowfishChecksizeAndPad(pt []byte) []byte {
   }
   return pt
 }
-
 func blowfishDecrypt(et, key []byte) []byte {
   dcipher, err := blowfish.NewCipher(key)
   if err != nil {
@@ -91,7 +65,6 @@ func blowfishDecrypt(et, key []byte) []byte {
   dcbc.CryptBlocks(decrypted, decrypted)
   return decrypted
 }
-
 func blowfishEncrypt(ppt, key []byte) []byte {
   ecipher, err := blowfish.NewCipher(key)
   if err != nil {
@@ -102,14 +75,48 @@ func blowfishEncrypt(ppt, key []byte) []byte {
   ecbc.CryptBlocks(ciphertext[blowfish.BlockSize:], ppt)
   return ciphertext
 }
-
 func decryptPass(pass, secretkey string) string {
   return string(blowfishDecrypt([]byte(pass), []byte(secretkey))[:])
 }
-
 func encryptPass(pass, secretkey string) string {
   return string(blowfishEncrypt(blowfishChecksizeAndPad([]byte(pass)), []byte(secretkey))[:])
 }
+
+func ksa(key []byte) []byte {
+  s := make([]byte, 256)
+  for i := range s {
+    s[i] = byte(i)
+  }
+  j := byte(0)
+  for i := range s {
+    j += s[i] + key[i%len(key)]
+    s[i], s[j] = s[j], s[i]
+  }
+  return s
+}
+func prga(message, s []byte) []byte {
+  text := make([]byte, len(message))
+  i, j := byte(0), byte(0)
+  for m := range message {
+    i++
+    j += s[i]
+    s[i], s[j] = s[j], s[i]
+    k := s[s[i]+s[j]]
+    text[m] = message[m] ^ k
+  }
+  return text
+}
+func rc4(message, key []byte) []byte { return prga(message, ksa(key)) }
+func encode(text, key string) string { return hex.EncodeToString(rc4([]byte(text), []byte(key))) }
+func decode(text, key string) string {
+  x, err := hex.DecodeString(text)
+  if err == nil {
+    return string(rc4(x, []byte(key)))
+  }
+  panic(err)
+}
+func encodeC(text string) string { return encode(text, _CHANGE) }
+func decodeC(text string) string { return decode(text, _CHANGE) }
 
 func getSecretKey(c int) string {
   secretkey, err := getpass.GetPassWithOptions("Secret Key: ", c, getpass.DefaultMaxPass)
@@ -124,16 +131,16 @@ func main() {
   switch kingpin.MustParse(app.Parse(os.Args[1:])) {
     case cGet.FullCommand():
       aciton = _GET
-      key = encode(string(*gKey))
+      key = encodeC(string(*gKey))
     case cNew.FullCommand():
       aciton = _NEW
-      key = encode(string(*nKey))
+      key = encodeC(string(*nKey))
     case cFind.FullCommand():
       aciton = _FIND
-      key = encode(string(*fKey))
+      key = encodeC(string(*fKey))
     case cDel.FullCommand():
       aciton = _DEL
-      key = encode(string(*dKey))
+      key = encodeC(string(*dKey))
     case cGen.FullCommand():
       fmt.Printf("%s\n", gopassgen.NewPassword(gopassgen.OptionLength(*gLen)))
       os.Exit(0)
@@ -148,14 +155,14 @@ func main() {
     }
   }
   ddb := dwarfdb.DwarfDBLoad(path, true)
-  if norKey = encode(os.Getenv("SGPM_NOR_KEY")); len(norKey) <= 0 {
-    norKey = encode("0x2b4674dca78cfde8d3d5a0d100996c941038ef71100")
+  if norKey = encodeC(os.Getenv("SGPM_NOR_KEY")); len(norKey) <= 0 {
+    norKey = encodeC("0x2b4674dca78cfde8d3d5a0d100996c941038ef71100")
   }
-  if securityKey = encode(os.Getenv("SGPM_KEY")); len(securityKey) <= 0 {
-    securityKey = encode("0x117498f0ea387cea4b00f77e8693ff9367a6L6")
+  if securityKey = encodeC(os.Getenv("SGPM_KEY")); len(securityKey) <= 0 {
+    securityKey = encodeC("0x117498f0ea387cea4b00f77e8693ff9367a6L6")
   }
-  if securityPass = encode(os.Getenv("SGPM_PASS")); len(securityPass) <= 0 {
-    securityPass = encode("0x27690b1e0e5bf93eb514035c824b85d3c274c")
+  if securityPass = encodeC(os.Getenv("SGPM_PASS")); len(securityPass) <= 0 {
+    securityPass = encodeC("0x27690b1e0e5bf93eb514035c824b85d3c274c")
   }
   pass, err := ddb.Get(securityKey)
   if err == nil {
@@ -193,10 +200,12 @@ func main() {
   }
   if aciton == _FIND {
     keys := ddb.GetAll()
+    key := decodeC(key)
     for i := 0; i < len(keys); i++ {
       ckey := keys[i]
-      if ckey != securityKey && ckey != norKey && strings.Contains(ckey, key) {
-        fmt.Printf("%s\n", decode(ckey))
+      dkey := decodeC(keys[i])
+      if ckey != securityKey && ckey != norKey && strings.Contains(dkey, key) {
+        fmt.Printf("%s\n", decodeC(ckey))
       }
     }
   } else if aciton == _GET {
